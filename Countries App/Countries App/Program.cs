e62a0da4996dd855600575c4e434024c00e70802
matthewfoucher <matthewfoucher@gmail.com>
2021-01-24 19:17:54 -0800

    Finished module 8. Finished Collections course.

---
+++ Countries App/Countries App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\matt\Documents\src\Training\Collections\Countries App\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Dictionary<string, List<Country>> countries = reader.ReadAllCountries();
+            foreach (string region in countries.Keys)
+                Console.WriteLine(region);
+
+            Console.Write("Which of the above regions do you want? ");
+            string chosenRegion = Console.ReadLine();
+
+            if (countries.ContainsKey(chosenRegion))
+            {
+                foreach (Country country in countries[chosenRegion].Take(10))
+                    Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            else
+                Console.WriteLine("That is not a valid region.");
+        }
+    }
+}
---
+++ Noughts And Crosses App/Noughts And Crosses App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Noughts_And_Crosses_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.PlayGame();
+            Console.WriteLine("Game over");
+        }
+    }
+}
